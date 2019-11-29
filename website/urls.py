456a1b5f48839cb5680ad5c4a08ee573ac64ea37
yassermej <yassermej50@gmail.com>
2019-11-29 08:51:55 -0500

    revise script

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^create_table/$', views.create_table, name="create_table"),
     url(r'^download_table/$', views.download_table, name="download_table"),
+    url(r'^error/$', views.error, name="error")
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 #-*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
 from django.db.models import Count
@@ -12,6 +12,7 @@ import uuid
 import json
 import operator
 import datetime
+from copy import deepcopy
 
 from lxml import etree
 from bs4 import BeautifulSoup
@@ -22,6 +23,16 @@ REQUIRED_FIELD = 'Effets'
 
 def merge_arrary_without_duplicate(arr1, arr2):
     return arr1 + list(set(arr2) - set(arr1))
+
+def get_real_index(rowspan_dict, idx):
+    total = 0
+    tmp_arr = deepcopy(rowspan_dict)
+    rowspan_idxs = rowspan_dict.keys()
+    for num in arr:
+        if idx > num:
+            total = total + 1
+
+    return idx - total
 
 # Create your views here.
 def home(request):
@@ -66,30 +77,77 @@ def create_table(request):
         if not has_ingredients:
             title_text = tree.xpath("//title/text()")[0].split(':')
             if len(title_text) > 0:
-                title = title_text[1].strip()
+                try:
+                    title = title_text[1].strip()
+                except:
+                    title = ''
 
             trows = tree.xpath("//table[@class='confluenceTable']//tbody//tr")
+            rowspan_fields = dict()
 
-            for row in trows:
+            for row in trows[1:]:
                 table_body = dict()
                 tr_tbodys = row.xpath(".//td")
+
                 if len(tr_tbodys) > 0:
-                    
+                    has_rowspan = False
+                    tmp_rowspan_fields = dict()
+                    for ttx, rowspan in enumerate(tr_tbodys):
+
+                        if 'rowspan' in rowspan.attrib:
+                            rowspan_count = int(rowspan.attrib['rowspan'])
+                            rowspan.set("rowspan", "")
+                            has_rowspan = True
+                            tmp_rowspan_fields[ttx+1] = {
+                                'body': etree.tostring(rowspan).decode('utf-8'),
+                                'count': rowspan_count
+                            }
+
+                    if has_rowspan:
+                        rowspan_fields = dict()
+                        rowspan_fields = tmp_rowspan_fields
+
                     for idx, th in enumerate(theads):
                         row_count = row.xpath(".//td[@class='confluenceTd']")
+                        td_xpath = ".//td[@class='confluenceTd'][{}]".format(idx+1)
                         if len(row_count) == len(theads):
                             td_xpath = ".//td[@class='confluenceTd'][{}]".format(idx+1)
+                            td_str = etree.tostring(row.xpath(td_xpath)[0]).decode('utf-8')
+
                         else:
-                            td_xpath = ".//td[@class='confluenceTd'][{}]".format(idx)
-                        if theads[idx] != SEARCH_KEY:
-                            td_str = ''
-                            if len(row.xpath(td_xpath)) > 0:
+                            total = 0
+                            tmp_arr = deepcopy(rowspan_fields)
+                            rowspan_idxs = rowspan_fields.keys()
+                            for num in rowspan_idxs:
+                                if (idx+1) > num:
+                                    total = total + 1
+
+                            if total == 0:
+                                try:
+                                    rowspan = deepcopy(rowspan_fields[idx+1])
+                                    if rowspan['count'] > 1:
+                                        rowspan['count'] = rowspan['count'] - 1
+                                        rowspan_fields[idx+1] = rowspan
+
+                                        td_str = rowspan['body']
+                                    else:
+                                        del rowspan_fields[idx+1]
+                                except Exception as e:
+                                    pass
+
+                            else:
+                                real_index = idx + 1 - total
+                                td_xpath = ".//td[@class='confluenceTd'][{}]".format(real_index)
                                 td_str = etree.tostring(row.xpath(td_xpath)[0]).decode('utf-8')
-                            table_body[theads[idx]] = td_str
+
+                        table_body[theads[idx]] = td_str
 
                     if REQUIRED_FIELD in table_body.keys():
                         effects_tree = etree.HTML(table_body[REQUIRED_FIELD])
-                        effects_value = effects_tree.xpath(".//text()")
+                        try:
+                            effects_value = effects_tree.xpath(".//text()")
+                        except:
+                            pass
 
                         if len(effects_value) > 0:
                             obj, created = TableRow.objects.get_or_create(title=title,
@@ -110,7 +168,7 @@ def create_table(request):
                 table_body = dict()
                 tr_tbodys = row.xpath(".//td")
                 if len(tr_tbodys) > 0:
-                    
+                    is_valid = True
                     for idx, th in enumerate(theads):
                         row_count = row.xpath(".//td[@class='confluenceTd']")
                         if len(row_count) == len(theads):
@@ -124,12 +182,17 @@ def create_table(request):
                             table_body[theads[idx]] = td_str
                         else:
                             row_count = row.xpath(".//td[@class='confluenceTd']")
+
                             if len(row_count) == len(theads):
-                                title = row.xpath(td_xpath)[0].xpath(".//text()")[0]
+                                try:
+                                    title = row.xpath(td_xpath)[0].xpath(".//text()")[0]
+                                except:
+                                    title = ""
+                                    is_valid = False
                             else:
                                 title = title
 
-                    if REQUIRED_FIELD in table_body.keys():
+                    if is_valid == True and REQUIRED_FIELD in table_body.keys():
                         effects_tree = etree.HTML(table_body[REQUIRED_FIELD])
                         effects_value = effects_tree.xpath(".//text()")
 
@@ -138,9 +201,9 @@ def create_table(request):
                                                                     body=json.dumps(table_body),
                                                                     guid=initial_guid)
                             if created:
-                                print('TRow: {} is created!'.format(title))
+                                print('TSIRow: {} is created!'.format(title))
                     else:
-                        print('TRow: {} has no effects!'.format(title))
+                        print('TSIRow: {} has no effects!'.format(title))
 
     TableHeader.objects.get_or_create(content=json.dumps(HEADERS))
     table_rows = TableRow.objects.values('title').order_by("title").distinct()
@@ -153,36 +216,45 @@ def download_table(request):
         return render(request, 'table.html', {'error': 'Method Get not allowed!'})
 
     selected = json.loads(request.body)
-    condition = Q(title__icontains=selected[0])
-    for filter_key in selected[1:]:
-        condition |= Q(title__icontains=filter_key)
-    results = TableRow.objects.filter(condition).order_by("title")
-    
-    headers = TableHeader.objects.filter()
-    header = None
-    if len(headers) == 0:
-        return render(request, 'home.html', {'error': 'Please analyze html files before downloa'})
 
-    table_rows = []
-    headers = json.loads(headers[0].content)
+    if len(selected) > 0:
+        condition = Q(title__icontains=selected[0])
+        for filter_key in selected[1:]:
+            condition |= Q(title__icontains=filter_key)
+        results = TableRow.objects.filter(condition).order_by("title")
+        
+        headers = TableHeader.objects.filter()
+        header = None
+        if len(headers) == 0:
+            return render(request, 'home.html', {'error': 'Please analyze html files before downloa'})
 
-    if SEARCH_KEY in headers:
-        headers.remove(SEARCH_KEY)
-    for result in results:
-        tmp_body = json.loads(result.body)
-        tmp_rows = []
-        for header in headers:
-            if header in tmp_body.keys():
-                tmp_rows.append(tmp_body[header])
-            else:
-                tmp_rows.append('<td class="confluenceTd"><p></p></td>')
+        table_rows = []
+        headers = json.loads(headers[0].content)
 
-        table_rows.append({
-            'title': result.title,
-            'body': tmp_rows
-        })
+        if SEARCH_KEY in headers:
+            headers.remove(SEARCH_KEY)
+        for result in results:
+            tmp_body = json.loads(result.body)
+            tmp_rows = []
 
-    return render(request, 'table.html', {'rows': table_rows,
-                                        'headers': headers,
-                                        'created': datetime.datetime.now(),
-                                        'range': range(len(headers))})
+            for header in headers:
+                if header in tmp_body.keys():
+                    tmp_rows.append(tmp_body[header])
+                else:
+                    tmp_rows.append('<td class="confluenceTd"><p></p></td>')
+
+            table_rows.append({
+                'title': result.title,
+                'body': tmp_rows
+            })
+
+
+        return render(request, 'table.html', {'rows': table_rows,
+                                            'headers': headers,
+                                            'created': datetime.datetime.now(),
+                                            'range': range(len(headers))})
+    else:
+        return redirect('/error')
+
+def error(request):
+    return render(request, 'error.html')
